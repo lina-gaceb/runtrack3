341bc52c9484d0575d764836a616ce6a090dffe5
lina-gaceb <92149888+lina-gaceb@users.noreply.github.com>
2022-04-24 06:01:01 +0200

    runtrack pas fini

---
+++ jour03/job01/script.js
@@ -0,0 +1,13 @@
+function apparaitre() {
+  $("#mydiv").append(
+    "<p>le $ est un bon élément monétaire. Le # de twitter est une bonne ID. Il faudra faire le point avec la classe pour cacher cet élément.</p>"
+  );
+}
+function cacher() {
+  $("#mydiv").empty();
+}
+$(document).ready(function () {
+  $("#button").click(apparaitre);
+
+  $("#buttonCacher").click(cacher);
+});
---
+++ jour03/job02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+
+<body>
+    <div id="melangees"></div>
+    <div id="rangees">
+        <img id="arc1" class="arc" src="png/arc1.png">
+        <img id="arc2" class="arc" src="png/arc2.png">
+        <img id="arc3" class="arc" src="png/arc3.png">
+        <img id="arc4" class="arc" src="png/arc4.png">
+        <img id="arc5" class="arc" src="png/arc5.png">
+        <img id="arc6" class="arc" src="png/arc6.png">
+    </div>
+    <button id="button">Mélanger</button>
+
+</body>
+
+</html>
